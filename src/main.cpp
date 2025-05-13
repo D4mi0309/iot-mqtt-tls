ed9629c5b18b2222c8b5971e598ab73503d51a87
D4mi0309 <damian.gil01@uceva.edu.co>
2025-05-13 17:35:53 -0500

    versiones firmware

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,7 @@ void setup() {
   startWiFi("");            // Paso 5. Inicializa el servicio de WiFi
   setupIoT();               // Paso 6. Inicializa el servicio de IoT
   hora = setTime();         // Paso 7. Ajusta el tiempo del dispositivo con servidores SNTP
+  Serial.println("Firmware version: v1.1.1"); // -- Muestra la version del firmware en el puerto serie
 }
 
 // Función loop
